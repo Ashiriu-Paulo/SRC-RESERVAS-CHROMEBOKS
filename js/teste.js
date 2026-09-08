fc7be7235aaab7b401ad0339e7786c38c0fc2ef5
Sarah-Paulo <paulo.rezende7@aluno.educa.go.gov.br>
2026-09-08 14:35:03 +0000

    conexão com o Banco

---
+++ js/teste.js
@@ -0,0 +1,12 @@
+import { supabase } from './config.js';
+
+async function listarDados() {
+    const { data, error, status, statusText } = await supabase
+        .from('usuarios')
+        .select('*');
+
+    console.log('STATUS:', status);
+    console.log('STATUS TEXT:', statusText);
+    console.log('DATA:', data);
+    console.log('ERROR:', error);
+}
